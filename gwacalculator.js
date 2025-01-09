9086d290fe7f8ef3414e4de974083e18ee8aa4d5
janrimureii <jnergaminghd@gmail.com>
2025-01-09 19:28:08 +0800

    modification

---
+++ gwacalculator.js
@@ -0,0 +1,4 @@
+let prelim = document.getElementById("prelimGrade");
+let midterm = document.getElementById("midtermGrade");
+let prefi = document.getElementById("prefiGrade");
+let finals = document.getElementById("finalsGrade");
