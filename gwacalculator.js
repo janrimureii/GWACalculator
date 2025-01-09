c0173a7cb53ad37c5f3a0bf58e6ee3870a652df3
janrimureii <jnergaminghd@gmail.com>
2025-01-09 20:13:04 +0800

    meh

--- gwacalculator.js
+++ gwacalculator.js
@@ -1,4 +1,23 @@
-let prelim = document.getElementById("prelimGrade");
-let midterm = document.getElementById("midtermGrade");
-let prefi = document.getElementById("prefiGrade");
-let finals = document.getElementById("finalsGrade");
+const prelim = parseFloat(document.getElementById("prelimGrade").value) || 0;
+const midterm = parseFloat(document.getElementById("midtermGrade").value) || 0;
+const prefi = parseFloat(document.getElementById("prefiGrade").value) || 0;
+const finals = parseFloat(document.getElementById("finalsGrade").value) || 0;
+
+console.log({prelim, midterm, prefi, finals});
+
+const calcBtn = document.getElementById("CalculateGWA");
+ 
+let displayGwa = document.getElementById("finalGwa");
+let displayRemarks = document.getElementById("remarks");
+
+calcBtn.addEventListener('click', calcfunc);
+
+function calcfunc() {
+
+    const gwa = parseFloat((prelim * 0.20) + (midterm * 0.20) + (prefi * 0.20) + (finals * 0.40));
+
+    displayGwa.innerHTML=gwa;
+    console.log("triggered ang function");
+    
+}
+
